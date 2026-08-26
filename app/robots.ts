3ac78992409051d57380447bdad3d4da00fcff9e
rahul <animenew122333@gmail.com>
2026-08-26 19:37:24 +0530

    robot file updated

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   const siteUrl =
     process.env.NEXT_PUBLIC_SITE_URL ||
-    "http://localhost:3000";
+    "https://www.lakshayfashioncollection.com";
 
   return {
     rules: {
